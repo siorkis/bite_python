750aacdc7d0e047e0869d76d9e7f9d82da3d6c43
Serghei Covtun <w.serghei.covtun@gmail.com>
2022-06-24 01:00:13 +0300

    hungman with optinional

--- hungman.py
+++ hungman.py
@@ -6,7 +6,7 @@ with open('sowpods.txt') as s:
 word = random.choice(words)
 answer = []
 game_word = []
-fail = 0
+guess_left = 6
 
 for char in word:
     answer.append(char)
@@ -14,33 +14,62 @@ for char in word:
 for i in range(len(word)):
     game_word.append('_')
 
+man = [    'O',
+      '-' ,'||', '-',
+        '| ', '| ']
+
+def print_man(man):
+    for j in range(len(man)):
+        if j == 0:
+            print('   '+ man[j], end=' ')
+        elif j == 1:
+            print()
+            print(man[j], end=' ')
+        elif j == 4:
+            print()
+            print(' ' + man[j], end=' ')
+        else:
+            print(man[j], end=' ')
+
 print(word)
 print(answer)
+print_man(man)
 
 while True:
-    letter = input("Guess the letter:\n")
-    letter = letter.upper()
+    print()
+    start = input("Start a new game? (yes):\n")
 
-    if letter in game_word:
-        print("You have already found that letter, try another:\n")
-    elif letter in answer:
-        indices = [i for i, x in enumerate(answer) if x == letter]
-        for index in indices:
-            game_word[index] = letter
-        if answer == game_word:
+    if start == "yes":
+        while True:
+            letter = input("Guess the letter:\n")
+            letter = letter.upper()
+
+            if letter in game_word:
+                print("You have already found that letter, try another:\n")
+            elif letter in answer:
+                indices = [i for i, x in enumerate(answer) if x == letter]
+                for index in indices:
+                    game_word[index] = letter
+                if answer == game_word:
+                    for char in game_word:
+                        print(char, end=' ')
+                    print("\nYou won")
+                    break
+            else:
+                print("There are no such letter, try again:\n")
+                guess_left -= 1
+                man.pop(-1)
+                print_man(man)
+                print()
+                # print("\nRemain", guess_left, "attempts")
+                if guess_left == 0:
+                    print("You loose")
+                    break
+
+            # print guessing word
             for char in game_word:
                 print(char, end=' ')
-            print("\nYou won")
-            break
+            print()
     else:
-        print("There are no such letter, try again:\n")
-        fail += 1
-        print("Remain", 6-fail, "attempts")
-        if fail == 6:
-            print("You loose")
-            break
+        break
 
-    # print guessing word
-    for char in game_word:
-        print(char, end=' ')
-    print()
