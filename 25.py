e7294a8ca7b9c124b4c05fca3d95b936e80908b9
Serghei Covtun <w.serghei.covtun@gmail.com>
2022-06-23 14:45:41 +0300

    added counter of guesses

--- 25.py
+++ 25.py
@@ -3,10 +3,12 @@ numbers = list(range(0, 101))
 length = len(numbers)
 index = length // 2
 print(numbers[index])
+guesses = 1
+
 while True:
     answer = input("Enter lower, higher or right: (say exit to end)\n")
     if answer == "right":
-        print("I found it")
+        print("I found it for", guesses, "guesses")
         break
     elif numbers[index] == numbers[0] or numbers[index] == numbers[-1]:
         print("I cannot find it")
@@ -14,8 +16,10 @@ while True:
     elif answer == "lower":
         index //= 2
         print(numbers[index])
+        guesses += 1
     elif answer == "higher":
         index = (length - index) // 2 + index
         print(numbers[index])
+        guesses += 1
     elif answer == "exit":
         break
